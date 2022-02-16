180abbaf37e20b8c55798e01a6220c073d1a0bee
William Whinn <w.willis.1986@gmail.com>
2022-02-16 16:20:57 +0000

    Include "rna_codon_table.py"

---
+++ seqconvert/rna_codon_table.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+"""
+Filename: rna_codon_table.py
+Author:   William Whinn
+
+RNA Codon table for translating tRNA into Protein.
+"""
+
+# Phe/F
+phenylalanine = ('UUU', 'UUC')
+
+# Leu/L
+leucine = ('UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG')
+
+# Ile/I
+isoleucine = ('AUU','AUC', 'AUA')
+
+# Met/M
+methionine = ('AUG')
+
+# Val/V
+valine = ('GUU', 'GUC', 'GUA', 'GUG')
+
+# Ser/S
+serine = ('UCU', 'UCC', 'UCA', 'UCG')
+
+# Pro/P
+proline = ('CCU', 'CCC', 'CCA', 'CCG')
+
+# Thr/T
+threonine = ('ACU', 'ACC', 'ACA', 'ACG')
+
+# Ala/A
+alanine = ('GCU', 'GCC', 'GCA', 'GCG')
+
+# Tyr/Y
+tyrosine = ('UAU', 'UAC')
+
+# His/H
+histidine = ('CAU', 'CAC')
+
+# Gln/Q
+glutamine = ('CAA', 'CAG')
+
+# Asn/N
+asparagine = ('AAU', 'AAC')
+
+# Lys/K
+lysine = ('AAA', 'AAG')
+
+# Asp/D
+aspartic_acid = ('GAU', 'GAC')
+
+# Glu/E
+glutamic_acid = ('GAA', 'GAG')
+
+# Cys/C
+cysteine = ('UGU', 'UGC')
+
+# Trp/W
+tryptophan = ('UGG')
+
+# Arg/R
+arginine = ('CGU', 'CGC', 'CGA', 'CGG')
+
+# Ser/S
+serine = ('AGU', 'AGC')
+
+# Arg/R
+arginine = ('AGA', 'AGG')
+
+# Gly/G
+glycine = ('GGU', 'GGC', 'GGA', 'GGG')
+
+# Stop Codons
+stop = ('UAA', 'UAG', 'UGA')
+
+# End of File.
