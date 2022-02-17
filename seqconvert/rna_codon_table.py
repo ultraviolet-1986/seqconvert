d749be498bcd78a2ac7ae7cdd906a71868089d46
William Whinn <w.willis.1986@gmail.com>
2022-02-17 16:36:42 +0000

    Place codons and proteins in dictionaries

--- seqconvert/rna_codon_table.py
+++ seqconvert/rna_codon_table.py
@@ -7,73 +7,42 @@ Author:   William Whinn
 RNA Codon table for translating tRNA into Protein.
 """
 
-# Phe/F
-phenylalanine = ('UUU', 'UUC')
+dict_rna_proteins = {
+    'A':['GCU', 'GCC', 'GCA', 'GCG'],                # A / Ala / Alanine
+    'C':['UGU', 'UGC'],                              # C / Cys / Cysteine
+    'D':['GAU', 'GAC'],                              # D / Asp / Aspartic Acid
+    'E':['GAA', 'GAG'],                              # E / Glu / Glutamic Acid
+    'F':['UUU', 'UUC'],                              # F / Phe / Phenylalanine
+    'G':['GGU', 'GGC', 'GGA', 'GGG'],                # G / Gly / Glycine
+    'H':['CAU', 'CAC'],                              # H / His / Histidine
+    'I':['AUU','AUC', 'AUA'],                        # I / Ile / Isoleucine
+    'K':['AAA', 'AAG'],                              # K / Lys / Lysine
+    'L':['UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'],  # L / Leu / Leucine
+    'M':['AUG'],                                     # M / Met / Methionine
+    'N':['AAU', 'AAC'],                              # N / Asn / Asparagine
+    'P':['CCU', 'CCC', 'CCA', 'CCG'],                # P / Pro / Proline
+    'Q':['CAA', 'CAG'],                              # Q / Gln / Glutamine
+    'R':['CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],  # R / Arg / Arginine
+    'S':['UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'],  # S / Ser / Serine
+    'T':['ACU', 'ACC', 'ACA', 'ACG'],                # T / Thr / Threonine
+    'V':['GUU', 'GUC', 'GUA', 'GUG'],                # V / Val / Valine
+    'W':['UGG'],                                     # W / Trp / Tryptophan
+    'Y':['UAU', 'UAC'],                              # Y / Tyr / Tyrosine
+    'stop':['UAA', 'UAG', 'UGA']                     # Stop Codons
+}
 
-# Leu/L
-leucine = ('UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG')
+# Reverse keys/values of 'dict_rna_proteins'.
+dict_rna_codons = {}
+for key,value in dict_rna_proteins.items():
+    for val in value:
+        if val in dict_rna_codons:
+            dict_rna_codons[val].append(key)
+        else:
+            dict_rna_codons[val] = [key]
 
-# Ile/I
-isoleucine = ('AUU','AUC', 'AUA')
-
-# Met/M
-methionine = ('AUG')
-
-# Val/V
-valine = ('GUU', 'GUC', 'GUA', 'GUG')
-
-# Ser/S
-serine = ('UCU', 'UCC', 'UCA', 'UCG')
-
-# Pro/P
-proline = ('CCU', 'CCC', 'CCA', 'CCG')
-
-# Thr/T
-threonine = ('ACU', 'ACC', 'ACA', 'ACG')
-
-# Ala/A
-alanine = ('GCU', 'GCC', 'GCA', 'GCG')
-
-# Tyr/Y
-tyrosine = ('UAU', 'UAC')
-
-# His/H
-histidine = ('CAU', 'CAC')
-
-# Gln/Q
-glutamine = ('CAA', 'CAG')
-
-# Asn/N
-asparagine = ('AAU', 'AAC')
-
-# Lys/K
-lysine = ('AAA', 'AAG')
-
-# Asp/D
-aspartic_acid = ('GAU', 'GAC')
-
-# Glu/E
-glutamic_acid = ('GAA', 'GAG')
-
-# Cys/C
-cysteine = ('UGU', 'UGC')
-
-# Trp/W
-tryptophan = ('UGG')
-
-# Arg/R
-arginine = ('CGU', 'CGC', 'CGA', 'CGG')
-
-# Ser/S
-serine = ('AGU', 'AGC')
-
-# Arg/R
-arginine = ('AGA', 'AGG')
-
-# Gly/G
-glycine = ('GGU', 'GGC', 'GGA', 'GGG')
-
-# Stop Codons
-stop = ('UAA', 'UAG', 'UGA')
+# Print items for verification.
+print(dict_rna_proteins)
+print()
+print(dict_rna_codons)
 
 # End of File.
