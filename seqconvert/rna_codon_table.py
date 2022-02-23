8091f4abbe0740644d5137c7f265ec2fee33aef5
William Whinn <w.willis.1986@gmail.com>
2022-02-23 16:46:50 +0000

    Correct rna_codon_table.py

--- seqconvert/rna_codon_table.py
+++ seqconvert/rna_codon_table.py
@@ -4,91 +4,86 @@
 Filename: rna_codon_table.py
 Author:   William Whinn
 
-RNA Codon table for translating tRNA into Protein.
+RNA Codon table for translating tRNA into Protein and back again.
 """
 
-dict_rna_proteins = {
-    'A':['GCU', 'GCC', 'GCA', 'GCG'],                # A / Ala / Alanine
-    'C':['UGU', 'UGC'],                              # C / Cys / Cysteine
-    'D':['GAU', 'GAC'],                              # D / Asp / Aspartic Acid
-    'E':['GAA', 'GAG'],                              # E / Glu / Glutamic Acid
-    'F':['UUU', 'UUC'],                              # F / Phe / Phenylalanine
-    'G':['GGU', 'GGC', 'GGA', 'GGG'],                # G / Gly / Glycine
-    'H':['CAU', 'CAC'],                              # H / His / Histidine
-    'I':['AUU','AUC', 'AUA'],                        # I / Ile / Isoleucine
-    'K':['AAA', 'AAG'],                              # K / Lys / Lysine
-    'L':['UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'],  # L / Leu / Leucine
-    'M':['AUG'],                                     # M / Met / Methionine
-    'N':['AAU', 'AAC'],                              # N / Asn / Asparagine
-    'P':['CCU', 'CCC', 'CCA', 'CCG'],                # P / Pro / Proline
-    'Q':['CAA', 'CAG'],                              # Q / Gln / Glutamine
-    'R':['CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],  # R / Arg / Arginine
-    'S':['UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'],  # S / Ser / Serine
-    'T':['ACU', 'ACC', 'ACA', 'ACG'],                # T / Thr / Threonine
-    'V':['GUU', 'GUC', 'GUA', 'GUG'],                # V / Val / Valine
-    'W':['UGG'],                                     # W / Trp / Tryptophan
-    'Y':['UAU', 'UAC'],                              # Y / Tyr / Tyrosine
-    'stop':['UAA', 'UAG', 'UGA']                     # Stop Codons
+DICT_PROTEIN_TO_CODON = {
+    'A':['GCU', 'GCC', 'GCA', 'GCG'],                  # A / Ala / Alanine
+    'C':['UGU', 'UGC'],                                # C / Cys / Cysteine
+    'D':['GAU', 'GAC'],                                # D / Asp / Aspartic Acid
+    'E':['GAA', 'GAG'],                                # E / Glu / Glutamic Acid
+    'F':['UUU', 'UUC'],                                # F / Phe / Phenylalanine
+    'G':['GGU', 'GGC', 'GGA', 'GGG'],                  # G / Gly / Glycine
+    'H':['CAU', 'CAC'],                                # H / His / Histidine
+    'I':['AUU','AUC', 'AUA'],                          # I / Ile / Isoleucine
+    'K':['AAA', 'AAG'],                                # K / Lys / Lysine
+    'L':['UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'],    # L / Leu / Leucine
+    'M':['AUG'],                                       # M / Met / Methionine
+    'N':['AAU', 'AAC'],                                # N / Asn / Asparagine
+    'P':['CCU', 'CCC', 'CCA', 'CCG'],                  # P / Pro / Proline
+    'Q':['CAA', 'CAG'],                                # Q / Gln / Glutamine
+    'R':['CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],    # R / Arg / Arginine
+    'S':['UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'],    # S / Ser / Serine
+    'T':['ACU', 'ACC', 'ACA', 'ACG'],                  # T / Thr / Threonine
+    'V':['GUU', 'GUC', 'GUA', 'GUG'],                  # V / Val / Valine
+    'W':['UGG'],                                       # W / Trp / Tryptophan
+    'Y':['UAU', 'UAC'],                                # Y / Tyr / Tyrosine
+    'stop':['UAA', 'UAG', 'UGA']                       # Stop Codons
 }
 
-# Reverse keys/values of 'dict_rna_proteins'.
-dict_rna_codons = {}
-for key,value in dict_rna_proteins.items():
+# Reverse keys/values of 'dict_protein_to_codon'.
+DICT_CODON_TO_PROTEIN = {}
+for key,value in DICT_PROTEIN_TO_CODON.items():
     for val in value:
-        if val in dict_rna_codons:
-            dict_rna_codons[val].append(key)
+        if val in DICT_CODON_TO_PROTEIN:
+            DICT_CODON_TO_PROTEIN[val].append(key)
         else:
-            dict_rna_codons[val] = [key]
-
-# Print items for verification.
-print(dict_rna_proteins)
-print()
-print(dict_rna_codons)
-print()
-
-sequence = "IVGGWECEQHSQPWQAALYHFSTFQCGGILVHRQWVLTAAHCISDNYQLWLGRHNLFDDENTAQFVHVSESFPHPGFNMSLLENHTRQADEDYSHDLMLLRLTEPADTITDAVKVVELPTEEPEVGSTCLASGWGSIEPENFSFPDDLQCVDLKILPNDECKKAHVQKVTDFMLCVGHLEGGKDTCVGDSGGPLMCDGVLQGVTSWGYVPCGTPNKPSVAVRVLSYVKWIEDTIAENS"
-
-# Break down a sequence into a parsable string.
-def split_sequence(sequence):
-    split_codons = []
-    codon = 3
-
-    for index in range(0, len(sequence), codon):
-        split_codons.append(sequence[index : index + codon])
-
-    print("\nReturned xNA sequence:")
-    print(split_codons)
+            DICT_CODON_TO_PROTEIN[val] = [key]
 
 
-split_sequence(sequence)
+def protein_to_trna(sequence):
+    """Convert Protein sequence to tRNA sequence."""
 
-
-
-def split_protein_sequence(sequence):
     split_codons = []
     n = 1
 
     for index in range(0, len(sequence), n):
         split_codons.append(sequence[index : index + n])
 
-    # print("\nReturned Protein sequence:")
-    # print(split_codons)
+    string = ""
 
-    return split_codons
+    for i in split_codons:
+        # Print first matching codon, remove '[0]' for all possible combos.
+        string += str(DICT_PROTEIN_TO_CODON[i][0])
+
+    print("\n", string)
 
 
-split = split_protein_sequence(sequence)
-print()
-print(split)
+def trna_to_protein(sequence):
+    """Convert tRNA sequence to Protein sequence."""
+    split_codons = []
+    codon = 3
 
-string = ""
+    for index in range(0, len(sequence), codon):
+        split_codons.append(sequence[index : index + codon])
 
-for i in split:
-    # Print first matching codon, remove '[0]' for all possible combos.
-    print(i, dict_rna_proteins[i][0])
-    string += str(dict_rna_proteins[i][0])
+    string = ""
 
-print(string)
+    for i in split_codons:
+        # Print first matching codon, remove '[0]' for all possible combos.
+        string += str(DICT_CODON_TO_PROTEIN[i][0])
 
+    print("\n", string)
+
+
+#############
+# Kickstart #
+#############
+
+sequence = "IVGGWECEQHSQPWQAALYHFSTFQCGGILVHRQWVLTAAHCISDNYQLWLGRHNLFDDENTAQFVHVSESFPHPGFNMSLLENHTRQADEDYSHDLMLLRLTEPADTITDAVKVVELPTEEPEVGSTCLASGWGSIEPENFSFPDDLQCVDLKILPNDECKKAHVQKVTDFMLCVGHLEGGKDTCVGDSGGPLMCDGVLQGVTSWGYVPCGTPNKPSVAVRVLSYVKWIEDTIAENS"
+protein_to_trna(sequence)
+
+sequence2 = "AUUGUUGGUGGUUGGGAAUGUGAACAACAUUCUCAACCUUGGCAAGCUGCUUUAUAUCAUUUUUCUACUUUUCAAUGUGGUGGUAUUUUAGUUCAUCGUCAAUGGGUUUUAACUGCUGCUCAUUGUAUUUCUGAUAAUUAUCAAUUAUGGUUAGGUCGUCAUAAUUUAUUUGAUGAUGAAAAUACUGCUCAAUUUGUUCAUGUUUCUGAAUCUUUUCCUCAUCCUGGUUUUAAUAUGUCUUUAUUAGAAAAUCAUACUCGUCAAGCUGAUGAAGAUUAUUCUCAUGAUUUAAUGUUAUUACGUUUAACUGAACCUGCUGAUACUAUUACUGAUGCUGUUAAAGUUGUUGAAUUACCUACUGAAGAACCUGAAGUUGGUUCUACUUGUUUAGCUUCUGGUUGGGGUUCUAUUGAACCUGAAAAUUUUUCUUUUCCUGAUGAUUUACAAUGUGUUGAUUUAAAAAUUUUACCUAAUGAUGAAUGUAAAAAAGCUCAUGUUCAAAAAGUUACUGAUUUUAUGUUAUGUGUUGGUCAUUUAGAAGGUGGUAAAGAUACUUGUGUUGGUGAUUCUGGUGGUCCUUUAAUGUGUGAUGGUGUUUUACAAGGUGUUACUUCUUGGGGUUAUGUUCCUUGUGGUACUCCUAAUAAACCUUCUGUUGCUGUUCGUGUUUUAUCUUAUGUUAAAUGGAUUGAAGAUACUAUUGCUGAAAAUUCU"
+trna_to_protein(sequence2)
 
 # End of File.
